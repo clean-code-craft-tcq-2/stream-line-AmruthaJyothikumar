a7eacc5b507664ed40fe93ed6fad39bd93f625b5
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-04-06 17:35:56 +0530

    Update SensorDataTransmission.h

--- SensorDataTransmission.h
+++ SensorDataTransmission.h
@@ -1,1 +1,9 @@
+void interpretTemperatureAndVoltageValuesFromSensors(parameterInfo tempInfo, parameterInfo voltInfo, int tempSensorValues[],
+	int VoltageSensorValues[], char* tempAndVoltValuesInCSV[], size_t numberOfSamples);
 
+float converttophysicalvalue(float resolution, int sensorvalue);
+void printtoconsoleTemp(float Temperature[], size_t numberOfSamples);
+void printtoconsoleVoltage((float Temperature[], size_t numberOfSamples);
+void interpretParameterValues(float resolution, int sensorValues[], int* interpretedParameterValues, size_t numberOfSamples);
+void GetSensorValues((int tempsensorValues[], float tempresolution, int voltagesensorValues[], float voltresolution);
+                     
