a31f444f953d7562bd27cff1ee2d06e11f3c3c32
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-04-09 11:42:46 +0530

    Delete SensorDataTransmission.h

--- SensorDataTransmission.h
+++
@@ -1,6 +0,0 @@
-
-extern int GetDatafromInputFile(char * filename, float *Temperature, float *Voltage);
-extern int isFileOpenSuccess(FILE *filePointer);
-extern int readDataAndSendToConsole(FILE *filePointer, float *Temperature, float *Voltage);
-extern int sendDataToConsole( float *Temperature, float *Voltage);
-                     
