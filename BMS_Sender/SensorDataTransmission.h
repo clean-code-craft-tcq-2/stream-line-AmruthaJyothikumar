0d47db8c5f5558c47e165ab500d9015b42054df3
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-04-09 13:05:31 +0530

    Update SensorDataTransmission.h

--- BMS_Sender/SensorDataTransmission.h
+++ BMS_Sender/SensorDataTransmission.h
@@ -1,3 +1,9 @@
+typedef enum
+{
+	E_NOT_OK,
+	E_OK
+}DATASTATUS;
+
 extern int GetDatafromInputFile(char * filename, float *Temperature, float *Voltage);
 extern int isFileOpenSuccess(FILE *filePointer);
 extern int readDataAndSendToConsole(FILE *filePointer, float *Temperature, float *Voltage);
