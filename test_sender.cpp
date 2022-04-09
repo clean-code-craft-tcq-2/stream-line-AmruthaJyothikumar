16980c9316fce4954d44c736bc61d7cb353ea3b7
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-04-09 11:00:38 +0530

    Create test_sender.cpp

---
+++ test_sender.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "sender.h"
+#include <string.h>
+
+TEST_CASE("Read Sensor 1 Data"){
+  int totalReadingsCaptured = 0;
+  float Temperature[50]={0};
+  float ChargeRate[50]={0};
+  char *filename = "./Sensor1_Data.txt";
+  totalReadingsCaptured = GetDatafromInputFile(filename,&Temperature[0],&Voltage[0]);
+  REQUIRE (totalReadingsCaptured == 50);
+  }
